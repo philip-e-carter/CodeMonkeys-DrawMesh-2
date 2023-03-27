e10f2818fa15acda4c3c0a8eb7fa94cc0f770046
philip e carter <philip.e.carter@gmail.com>
2023-03-27 09:39:32 -0500

    Its drawing for Christ's sake!

--- Assets/DrawMesh.cs
+++ Assets/DrawMesh.cs
@@ -36,8 +36,77 @@ public class DrawMesh : MonoBehaviour {
         GetComponent<MeshFilter>().mesh = mesh;
         // GetComponent<MeshFilter>().mesh = UtilsClass.InitMesh();
         mesh.MarkDynamic(); // if moved to Awake(), nothing is drawn. hmm maybe this is the solution for me.
-
+        DrawSomethingForChristsSake();
         // lastMousePosition = mouseWorldPosition;
+    }
+
+    private void DrawSomethingForChristsSake() {
+
+        Vector3[] vertices = new Vector3[6];
+        Vector2[] uv = new Vector2[6];
+        int[] triangles = new int[9];
+        
+        vertices[0] = new Vector3(0,0,0);
+        vertices[1] = new Vector3(0,1000,0);
+        vertices[2] = new Vector3(1000,1000,0);
+        vertices[3] = new Vector3(1000,0,0);
+        vertices[4] = new Vector3(1000,2000,0);
+        vertices[5] = new Vector3(2000,100,0);
+        
+        uv[0] = Vector2.zero;
+        uv[1] = Vector2.zero;
+        uv[2] = Vector2.zero;
+        uv[3] = Vector2.zero;
+        uv[4] = Vector2.zero;
+        uv[5] = Vector2.zero;
+
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+        triangles[3] = 3;
+        triangles[4] = 4;
+        triangles[5] = 5;
+        // triangles[6] = 6;
+        // triangles[7] = 7;
+        // triangles[8] = 8;
+        
+        // mesh.vertices.CopyTo(vertices, 0);
+        // mesh.uv.CopyTo(uv, 0);
+        // mesh.triangles.CopyTo(triangles, 0);
+
+        // int vIndex = vertices.Length - 4;
+        // int vIndex0 = vIndex + 0;
+        // int vIndex1 = vIndex + 1;
+        // int vIndex2 = vIndex + 2;
+        // int vIndex3 = vIndex + 3;
+
+        // Vector3 mouseForwardVector = (mouseWorldPosition - lastMousePosition).normalized;
+        // Vector3 normal2D = new Vector3(0, 0, -1f);
+        // float lineThickness = 0.2f;
+        // Vector3 newVertexUp = mouseWorldPosition + Vector3.Cross(mouseForwardVector, normal2D) * lineThickness;
+        // Vector3 newVertexDown = mouseWorldPosition + Vector3.Cross(mouseForwardVector, normal2D * -1f) * lineThickness;
+
+        // vertices[vIndex2] = newVertexUp;
+        // vertices[vIndex3] = newVertexDown;
+
+        // uv[vIndex2] = Vector2.zero;
+        // uv[vIndex3] = Vector2.zero;
+
+        // int tIndex = triangles.Length - 6;
+
+        // triangles[tIndex + 0] = vIndex0;
+        // triangles[tIndex + 1] = vIndex2;
+        // triangles[tIndex + 2] = vIndex1;
+
+        // triangles[tIndex + 3] = vIndex1;
+        // triangles[tIndex + 4] = vIndex2;
+        // triangles[tIndex + 5] = vIndex3;
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+
+        lastMousePosition = mouseWorldPosition;
     }
 
     private void Update() {
